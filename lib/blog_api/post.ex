2cfb1136bd8b6bb7999669e34ebf356a261f0dc2
mubeenghauri <ghauri.mubeen@gmail.com>
2022-05-27 11:27:14 +0500

    Post controller and model

--- lib/blog_api/post.ex
+++ lib/blog_api/post.ex
@@ -1,18 +1,109 @@
 defmodule BlogApi.Post do
   use Ecto.Schema
+  alias BlogApi.{Repo, Post, User}
+  require Logger
   import Ecto.Changeset
+	import Ecto.Query, only: [from: 2]
 
+
+  # note to self: adding the suffix 'a' gives a list of atom
+  @required_registration_fields ~w(content title user_id)a
+
+  @derive {Jason.Encoder, only: [:id, :title, :content, :user]}
   schema "posts" do
     field :content, :string
+    field :title, :string
+    field :user_id, :integer
+
+    belongs_to :user, User, define_field: :false
 
     timestamps()
   end
 
   @doc false
+  def creation_changeset(post, attrs) do
+    post
+    |> cast(attrs, [:content, :title, :user_id])
+    |> validate_required(@required_registration_fields)
+    |> validate_length(:content, min: 8)
+    |> validate_length(:title, min: 8)
+  end
+
   def changeset(post, attrs) do
     post
-    |> cast(attrs, [:content])
-    |> validate_required([:content])
-    |> validate_length(:password, min: 8)
+    |> cast(attrs, [:content, :title, :user_id ])
+    |> validate_length(:content, min: 8)
+    |> validate_length(:title, min: 8)
   end
+
+  # def by( params ) do
+  #   """
+  #     resource = __MODULE__ |> Repo.get etc...
+  #     ???
+  #   """
+  #   post =
+  #     Post
+  #     |> Repo.get_by( params )
+
+  #   case post do
+  #     nil ->
+  #       {:error, :nopost}
+  #     %BlogApi.Post{} ->
+  #       {:ok, post}
+  #   end
+  # end
+
+  def all_by_user( user_id ) do
+    {user_id, _} = Integer.parse(user_id)
+    post = Repo.all( from p in "posts",
+                    where: p.user_id == ^user_id,
+                    select: [:title, :content]  )
+
+    case post do
+      nil ->
+        {:error, :nopost}
+      _ ->
+        {:ok, post}
+    end
+  end
+
+  def post_by_user(user_id, post_id) do
+    {user_id, _} = Integer.parse(user_id)
+    {post_id, _} = Integer.parse(post_id)
+    post = Repo.all( from p in "posts",
+                    where: p.user_id == ^user_id,
+                    where: p.id == ^post_id,
+                    select: [:title, :content])
+
+    case post do
+      nil ->
+        {:error, :nopost}
+      _ ->
+        {:ok, post}
+    end
+  end
+
+  def delete(user_id, post_id) do
+    {user_id, _} = Integer.parse(user_id)
+    {post_id, _} = Integer.parse(post_id)
+
+    try do
+      Post
+      |> Repo.get_by!(user_id: user_id, id: post_id)
+      |> Repo.delete
+
+      :ok
+    rescue
+      e ->
+        Logger.error "Failed deleting post"
+        Logger.error e
+
+        :error
+    end
+  end
+
+  # defp get_by(queryable, clauses, opts \\ []) do
+  #   # use Ecto.Repo
+  #   Ecto.Repo.Queryable.all(__MODULE__, Ecto.Repo.query_for_get_by(queryable, clauses), opts)
+  # end
 end
---
+++ lib/blog_api_web/controllers/post_controller.ex
@@ -0,0 +1,66 @@
+defmodule BlogApiWeb.PostController do
+  require Logger
+  use BlogApiWeb, :controller
+  alias BlogApi.{Repo, Post, Paginator}
+  use BlogApiWeb.Controller.ErrorHandler
+	import Ecto.Query, only: [from: 2]
+
+
+  def index(conn, %{"user_id" => user_id}) do
+    # IO.inspect (param)
+    with {:ok, posts} <- Post.all_by_user(user_id) do
+      render(conn, "posts.json", posts: posts)
+    else
+      {:error, :nopost} ->
+        render(conn, "posts.json", posts: [])
+    end
+  end
+
+  def paginated(conn, %{"page" => page}) do
+    {page, _} = Integer.parse(page)
+    posts =
+        from p in Post,
+        join: u in assoc(p, :user),
+        preload: [user: u]
+        # select: [p.id, p.title, p.content]
+
+    paginated_post = Paginator.paginate(posts, page)
+
+    Logger.debug paginated_post
+    render conn, "paginate.json", paginated: paginated_post
+  end
+
+  def create(conn, %{"user_id" => _user_id,
+                     "title" => _title, "content" => _content} = param) do
+
+    with {:ok, changeset} <- {:ok, Post.creation_changeset(%Post{}, param)},
+         {:ok, changeset} <- Repo.validate_changeset(changeset),
+         {:ok, %Post{} = _post} <- Repo.insert(changeset) do
+          render(conn, "ok.json", msg: "post created successfully")
+    else
+      # TODO: enclose below matching to function? (handle_creation_error/1 maybe?)
+      error ->
+        handle_creation_errors(conn, error)
+    end
+  end
+
+  def show(conn, %{"user_id" => user_id, "id" => post_id}) do
+    with {:ok, post} <- Post.post_by_user(user_id, post_id)
+    do
+      render(conn, "posts.json", posts: post)
+    else
+      _ ->
+        render(conn, "error.json", msg: "no post found")
+    end
+  end
+
+  def delete(conn,  %{"user_id" => user_id, "id" => post_id}) do
+    case Post.delete(user_id, post_id) do
+      :ok ->
+        render(conn, "ok.json", msg: "post deleted successfully")
+      :error ->
+        render(conn, "error.json", msg: "failed deleting post. are u sure this post exists?")
+    end
+  end
+
+end
